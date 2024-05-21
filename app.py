97cca65d1c24d3dfd421adae0b0f4eee479a8c87
LiTe1991 <vadim19917@gmail.com>
2024-05-21 17:12:50 +0200

    chg: add light support and start stop proc, but timer start not working

--- app.py
+++ app.py
@@ -3,11 +3,11 @@
 """
 import datetime
 import sys
+import threading
 
 from PySide6.QtCore import QTimer
 from PySide6.QtWidgets import QApplication, QMainWindow
-from gpiozero import Button, MotionSensor, LineSensor
-from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import MotionSensor, RGBLED
 
 import view.RaceView as raceView
 
@@ -16,6 +16,10 @@ class MainWindow(QMainWindow):
     """
         Main Window contain logic for loading race ui and register actions for timer and gpios
     """
+    red_light = (1, 0, 0)
+    green_light = (0, 1, 0)
+    no_light = (1, 1, 1)
+
     def __init__(self):
         super().__init__()
         self.ui = raceView.Ui_MainWindow()
@@ -26,60 +30,52 @@ class MainWindow(QMainWindow):
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_stopwatch_time)
 
-        self.button_is_checked = True
+        self.button_is_checked = False
         self.ui.actionButton.setCheckable(True)
         self.ui.actionButton.released.connect(self.trigger_timer_action)
         self.ui.actionButton.setChecked(self.button_is_checked)
 
-        self.factory = PiGPIOFactory(host='192.168.178.44')
-        self.gpio = MotionSensor(22, pin_factory=self.factory)
-
-        self.gpio.when_motion = self.trigger_timer_action
-
-        # elapsed_time = datetime.datetime.now() - measured_time
-        # def trigger_timer_action(self):
-        #    """
-        #        Trigger the action for timer, make timer ready or abort proc
-        #    """
-        #    print(self.gpio.is_pressed)
-        #    if self.start_time is None:
-        #        self.ui.actionButton.setText("Abbrechen")
-        #        self.timer.start()
-        #        self.start_time = datetime.datetime.now()
-        #    else:
-        #        self.ui.actionButton.setText("Ready")
-        #        self.timer.stop()
-        #        self.start_time = None
+        self.gpio = MotionSensor(22, queue_len=10)  # Set the queue length trigger something like sensitivity
+        self.led = RGBLED(red=23, green=24, blue=25, initial_value=self.red_light)
 
     def trigger_timer_action(self):
         """
             Trigger the action for timer, make timer ready or abort proc
         """
-        #print('start_timer')
-        print('GPIO:' + str(self.gpio.is_active))
-        print('GPIO3:' + str(self.gpio.active_time))
+        if self.button_is_checked:
+            self.ui.actionButton.setText("Ready")
+            self.button_is_checked = False
+            self.timer.stop()
+            self.led.value = self.red_light
+            self.gpio.when_motion = None
+        else:
+            self.ui.actionButton.setText("Stop")
+            self.button_is_checked = True
+            self.led.value = self.green_light
+            self.gpio.when_motion = self.trigger_start_timer
 
-        #if self.button_is_checked:
-        #    self.stop_timer()
-        #else:
-        #    self.start_time = datetime.datetime.now()
-        #    self.timer.start(50)
-        #    self.ui.actionButton.setText("Stop")
+    def trigger_start_timer(self):
+        """
+            Trigger the action for timer, make timer ready or abort proc
+        """
+        self.start_time = datetime.datetime.now()
+        print('Start time: ' + str(self.start_time))
+        self.led.value = self.no_light
+        self.timer.start(100)
+        self.gpio.when_motion = self.trigger_stop_timer
 
-    def stop_timer(self):
+    def trigger_stop_timer(self):
         """
             Test
         """
-        print('stop_timer')
+        print('Stop time')
         self.timer.stop()
         self.update_stopwatch_time()
-        self.ui.actionButton.setText("Ready")
 
     def update_stopwatch_time(self):
         """
             Update the timer label and set actual measured time
         """
-        print('update_Test')
         measured_time = datetime.datetime.now() - self.start_time
         print(str(datetime.datetime.now()) + " " + str(self.start_time))
         print('Update:' + str(measured_time))
